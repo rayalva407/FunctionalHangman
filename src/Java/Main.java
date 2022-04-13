59cb1fcec5935c8f999c1f1729305b9adee0e0ea
Ray <ray@pop-os.localdomain>
2022-04-12 23:24:03 -0600

    Adds the ability to choose to play again and writes score to file and checks if score was high score

--- src/Java/Main.java
+++ src/Java/Main.java
@@ -14,36 +14,44 @@ Limitations:
  */
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
         importData();
+        System.out.println(scoreBoard);
         titleScreen();
         getUserName();
         getSecretWord();
         //game loop
         while (playing) {
-            System.out.println(secretWord);
-            System.out.println(score);
             updateGameState();
             updateWrongGuesses();
             updateWordState();
+            System.out.println("Score: " + score);
             takeGuess();
             checkGuess();
             if (winner()) {
                 score+=50;
+                updateGameState();
+                updateWordState();
+                System.out.println("Score: " + score);
                 System.out.println("You win!");
-                System.out.println("Would you like to play again?");
+                playAgain();
             }
-            System.out.println(currentState);
+            if (loser()) {
+                updateGameState();
+                updateWordState();
+                System.out.println("Score: " + score);
+                System.out.println("You Lose!");
+                playAgain();
+            }
         }
 
 
@@ -90,6 +98,7 @@ public class Main {
     //Gets the user's name
     private static String getUserName() {
         userName = scanner.nextLine();
+        System.out.println("Hello " + userName + "!");
         return userName;
     }
 
@@ -119,7 +128,7 @@ public class Main {
 
     //Takes a guess from user
     private static String takeGuess() {
-        System.out.println("Please take a guess:");
+        System.out.println("Please make a guess:");
         try {
             guess = String.valueOf(scanner.nextLine().charAt(0));
         }
@@ -144,15 +153,73 @@ public class Main {
         }
     }
 
+    //Updates the list of wrong guesses
     private static void updateWrongGuesses() {
         System.out.print("Incorrect guesses: ");
         incorrectList.stream().forEach(ch -> System.out.print(ch + " "));
         System.out.println();
     }
 
+    //Checks to see if the player won
     private static boolean winner() {
         int correctGuesses = (int) Arrays.stream(secretWord.split("")).filter(ch -> correctList.contains(ch)).count();
         return correctGuesses == secretWord.length();
     }
+
+
+    private static boolean loser() {
+        return currentState == 7;
+    }
+
+    private static void playAgain() {
+        System.out.println("Would you like to play again?");
+        if (scanner.nextLine().equals("y")) {
+            correctList.clear();
+            incorrectList.clear();
+            if (loser()) score = 0;
+            currentState = 0;
+            getSecretWord();
+        }
+        else {
+            if (checkHighScore()) {
+                System.out.println("Congratulations you got the high score!");
+            }
+            writeScoreToFile();
+            System.out.println("Thank you for playing! Goodbye!");
+            playing = false;
+        }
+    }
+
+    private static void writeScoreToFile() {
+        String entry;
+        try {
+            Path path = Path.of("src/Resources/scoreboard.txt");
+            if (Files.size(path) == 0) {
+                entry = String.format("%s:%s", userName, score);
+            }
+            else {
+                entry = String.format("%s%s:%s", System.lineSeparator(), userName, score);
+            }
+            Files.write(path, entry.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+        }
+        catch (Exception e) {
+            System.out.println("File not found");
+        }
+    }
+
+    private static boolean checkHighScore() {
+        if (scoreBoard.size() == 0) return true;
+
+        List<Integer> scoreList = scoreBoard.stream()
+                .map(str -> str.split(":"))
+                .map(arr -> Integer.parseInt(arr[1]))
+                .collect(Collectors.toList());
+        int maxNumber = scoreList.stream()
+                .max(Integer::compareTo)
+                .get();
+        return score > maxNumber;
+    }
+
+
 }
 
